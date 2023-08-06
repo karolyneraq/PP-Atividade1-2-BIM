48e2cc152b2a5f4565964cde6437671f2ed596f4
Dário Souza <drio.srf@gmail.com>
2023-08-06 19:59:16 -0400

    Update Autor.java

--- src/main/java/data/Autor.java
+++ src/main/java/data/Autor.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package data;
 
-/**
- *
- * @author karol
- */
+import java.util.ArrayList;
+
 public class Autor {
-    
+	private String nome;
+	private String nacionalidade;
+	private ArrayList<Livro> livros;
+
+	public Autor(String nome, String nacionalidade) {
+		this.nome = nome;
+		this.nacionalidade = nacionalidade;
+		this.livros = new ArrayList<>();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+
+	public ArrayList<Livro> getLivros() {
+		return livros;
+	}
+
+	public void adicionarLivro(String nome) {
+		livros.add(new Livro(nome, this));
+	}
+
+	public void removerLivro(Livro livro) {
+		livros.remove(livro);
+	}
+
+	public void listarLivros(){
+		
+		if (livros.size() > 0){
+			
+			String saida = "O autor " + this.nome + " escreveu estes livros:\n";
+			
+			for(int i = 0; i < this.livros.size(); i++){
+				saida += "Titulo: " + this.livros.get(i).getTitulo() + "\n\n";
+			}
+			
+			System.out.println(saida);
+		}
+		
+		else {
+			System.out.println("O autor " + this.getNome() + " ainda não escreveu nenhum livro");
+		}
+	}
+	
 }
