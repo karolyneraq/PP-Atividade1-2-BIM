59f38e46d583d4d9e5aca10a7c5ed74403dd1807
Dário Souza <drio.srf@gmail.com>
2023-08-06 20:00:10 -0400

    Update Livro.java

--- src/main/java/data/Livro.java
+++ src/main/java/data/Livro.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package data;
 
-/**
- *
- * @author karol
- */
 public class Livro {
-    
+	private String titulo;
+	private Autor autor;
+	private Editora editora;
+
+	public Livro(String titulo, Autor autor) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.setEditora(null);
+	}
+	
+	public Livro(String titulo, Autor autor, Editora editora) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.setEditora(editora);
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public Editora getEditora() {
+		return editora;
+	}
+
+	public void setEditora(Editora editora) {
+		this.editora = editora;
+	}
 }
